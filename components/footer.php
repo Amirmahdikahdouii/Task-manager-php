7440cd8fb919a870538363e5249d6c94819d5024
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-08-08 20:40:16 +0330

    Update components and asset images

---
+++ components/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="footer-bottom">
+        <p>&copy; 2024 Task Manager. All rights reserved.</p>
+    </div>
+</footer>
---
+++ components/header.php
@@ -0,0 +1,23 @@
+<!-- Header Section -->
+<header class="site-header">
+    <div class="header-container">
+        <div class="header-brand">
+            <a href="/task-manager/index.php" class="header-brand-name">Task Manager</a>
+        </div>
+        <nav class="header-main-nav">
+            <ul>
+                <li><a href="/task-manager/index.php" class="nav-link">Home</a></li>
+                <li><a href="#" class="nav-link">Tasks</a></li>
+                <li><a href="#" class="nav-link">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="auth-buttons">
+            <a href="#" class="btn login-btn">Login</a>
+            <a href="/task-manager/users/signUp.php"
+               class="btn signup-btn">
+                Sign Up
+            </a>
+        </div>
+    </div>
+</header>
+<!-- End Header section -->
